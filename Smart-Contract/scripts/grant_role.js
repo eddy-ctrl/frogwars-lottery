b35edad86ffe6db20b0e297b142777f833c5e9c5
Medet Ahmetson <milayter@gmail.com>
2024-05-05 22:14:01 +0700

    Fix granting roles

--- Smart-Contract/scripts/grant_role.js
+++ Smart-Contract/scripts/grant_role.js
@@ -8,9 +8,13 @@ async function main() {
     const Lottery    = await ethers.getContractFactory("DecentralizedLottery");
     const lotteryAddress             = contractAddresses[chainID][0];
     const lottery           = await Lottery.attach(lotteryAddress);
+    const adminRole                 = "0x0000000000000000000000000000000000000000000000000000000000000000";
 
-    let tx = await lottery.performUpkeep(0x00, {from: deployer.address});
-    console.log(`Lottery End (KeepUp Performed): ${tx.hash}`);
+    let tx = await lottery.grantRole(adminRole, "0x319a59ac483A52A4ed3c82Ed1B8B6883BFA80139", {from: deployer.address});
+    console.log(`Grant Role: ${tx.hash}`);
+
+    tx = await lottery.revokeRole(adminRole, deployer.address, {from: deployer.address});
+    console.log(`Revoke Role: ${tx.hash}`);
 }
 
 main()
